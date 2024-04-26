7be32f90d4767d6e1cf09baff35831e392331d42
hkrieger <henry.krieger@campus.tu-berlin.de>
2024-04-26 17:32:45 +0200

    website setup

---
+++ src/views/AppView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="apps">
+        <div class="applist">
+            <ul>
+                <li><h2>University Wizard</h2></li>
+                <li><h2>Trick Counting - Point Tracker</h2></li>
+                <li><h2>HLG-KAIFU App</h2></li>
+            </ul>
+        </div>
+    </div>
+</template>
+  
